3f0f839e39d0aa9bdd269c62286133b7b898af5c
varsha1076 <vparmar1076@gmail.com>
2025-11-15 15:46:09 +0530

    day-12

--- hms/src/App.jsx
+++ hms/src/App.jsx
@@ -1,10 +1,28 @@
-function App(){
-  return(
-    <>
-    <home/>
-    <about/>
-    </>
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Header from './layout/Header'
+import About from './pages/About'
+import Blog from './pages/Blog'
 
+export default function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}>
+      <Route path='/about' element={<About/>}>
+      </Route>
+      <Route path='/blog' element={<Blog/>}>
+      </Route>
+      <Route path='/contact' element={<Contact/>}>
+      </Route>
+
+      </Route>
+    </Routes>
+    </BrowserRouter>
+    </>
+   
   )
 }
-export default App;
